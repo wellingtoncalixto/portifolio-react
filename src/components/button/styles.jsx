95deeb4ed96042992ad241c90c5a329c08febc55
Wellington Calixto <wac.monti@outlook.com>
2024-03-11 15:27:24 -0300

    Refatoração estilos do ButtonComponent

--- src/components/button/styles.jsx
+++ src/components/button/styles.jsx
@@ -1,7 +1,53 @@
 import styled, { css } from "styled-components";
 
-const buttonStyles = ({ $buttonStyle, theme }) => {};
+const communButtonStyles = ({ theme }) => {
+  const padrao = css`
+    text-decoration: none;
+    width: max-content;
+    padding: ${theme.space._8} ${theme.space._16};
+    font-weight: ${theme.font_weight.bold};
+    border-radius: ${theme.space._12};
 
-export const LinkButton = styled.a``;
+    @media screen and (min-width: 768px) {
+      padding: ${theme.space._12} ${theme.space._24};
+    }
+  `;
+  return padrao;
+};
 
-export const NormalButton = styled.button``;
+const buttonStyles = ({ $buttonStyle, theme }) => {
+  const primary = css`
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.b8};
+    border: ${theme.space._4} solid ${theme.colors.b8};
+
+    &:hover {
+      background: linear-gradient(${theme.colors.b8}, ${theme.colors.b7});
+    }
+  `;
+
+  const secondary = css`
+    background: transparent;
+    color: ${theme.colors.b8};
+    border: ${theme.space._4} solid ${theme.colors.b8};
+
+    &:hover {
+      color: ${theme.colors.b7};
+      border-color: ${theme.colors.b7};
+    }
+  `;
+  return (
+    ($buttonStyle === "primary" && primary) ||
+    ($buttonStyle === "secondary" && secondary)
+  );
+};
+
+export const LinkButton = styled.a`
+  ${(props) => communButtonStyles(props)}
+  ${(props) => buttonStyles(props)}
+`;
+
+export const NormalButton = styled.button`
+  ${(props) => communButtonStyles(props)}
+  ${(props) => buttonStyles(props)}
+`;
